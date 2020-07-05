34484518e3eb62497d73acff8cea577f99aafe23
saivythik <60150574+saivythik@users.noreply.github.com>
2020-07-05 17:39:08 +0530

    Update app.js

--- assignment 1/js/app.js
+++ assignment 1/js/app.js
@@ -1,16 +1,34 @@
 (function (){
-	'use strict';
-	angular.module("LunchCheck",[]).controller("LunchCheckController",LunchCheckController);
-	LunchCheckController.$inject=['$scope'];
-	function LunchCheckController($scope){
-	$scope.message="";
-	$scope.str="";
-	$scope.checkf=function(){
-		var arr=$scope.str.split(',');
-		$scope.result=arr.filter(arr=> arr.trim.length > 0)
-		$scope.alen=$scope.result.length;
-		$scope.message=$scope.result.length ? $scope.length>3 "Too much!":"Enjoy!":"Please enter data first";
-	};
+'use strict';
+angular.module("myapp",[]).controller("mycontroller",mycontroller);
+mycontroller.$inject=['$scope'];
+function mycontroller($scope){
+  $scope.messages= "";
+  $scope.stringarr="";
+  $scope.checkstr = function (){
+    var arr= $scope.stringarr.split(',');
+    $scope.result = arr.filter(arr => arr.trim().length > 0);
+    $scope.arrlength = $scope.result.length;
+  //   if(result.length === 0)
+  //   {
+  //     $scope.messages="Please enter the data first";
+  //     return 0;
+  //   }
+  //   else{
+  //   if(result.length <=3 )
+  //   {
+  //      $scope.messages = "Enjoy!";
+  //      return 1;
+  //   }
+  //   if(result.length > 3)
+  //   {
+  //      $scope.messages = "Too Much!";
+  //      return 2;
+  //   }
+  // }
+  $scope.messages = $scope.arrlength ? $scope.arrlength >3 ? 'Too
+much!' : 'Enjoy!' : 'Please enter data first';
+  };
 }
-
 })();
+
